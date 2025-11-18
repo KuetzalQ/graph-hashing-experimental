9d67a6d7fee310b0fe77fa6db5fd1c8327790ad1
asdfghjkl-alt <56777557+asdfghjkl-alt@users.noreply.github.com>
2025-11-19 10:51:17 +1100

    Time complexity of algorithm test

---
+++ time-complexity.py
@@ -0,0 +1,84 @@
+# Implementing the method of this paper: https://link.springer.com/chapter/10.1007/978-981-15-5341-7_25
+import networkx as nx
+import GraphUtils
+import HashUtils
+from random import shuffle, choice
+import matplotlib.pyplot as plt
+import time
+from networkx.algorithms.graph_hashing import weisfeiler_lehman_graph_hash
+
+def shingle(g: nx.Graph):
+    nodes = list(g.nodes())
+    shingles = []
+    for node in nodes:
+        shingle = []
+        for neighbour in g.neighbors(node):
+            shingle.append(g.degree(neighbour))
+        shingles.append(tuple(sorted(shingle)))
+    # doesn't matter whether shingles are sorted or not
+    return shingles
+
+test_v_size = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000]
+average_time = []
+false_pos = []
+
+for no_v in test_v_size:
+    graphs = []
+    # Running it on 500 graphs to prevent biases in average
+    n = 50
+
+    for i in range(n):
+        # g = nx.gnp_random_graph(no_v, 0.6)
+        g = nx.gnm_random_graph(no_v, no_v * 100)
+        g.name = f"graph_{i}"
+        graphs.append(g.copy())
+        
+    original = choice(graphs)
+    duplicate = GraphUtils.randomise_labels(original)
+    graphs.append(duplicate.copy())  # add a relabelled, duplicated graph
+    duplicate = GraphUtils.randomise_labels(original)
+    graphs.append(duplicate.copy())  # add a relabelled, duplicated graph
+    shuffle(graphs)
+
+    start = time.perf_counter()
+
+    signatures = {}
+    for g in graphs:
+        shingles = shingle(g)
+        signature = HashUtils.minhash(set(shingles), num_hashes=64)
+        if signature not in signatures:
+            signatures[signature] = []
+        signatures[signature].append(g)
+
+    fps = 0
+    end = time.perf_counter()
+
+    average_time.append(end - start)
+    print(end - start)
+
+    for sig in signatures:
+        if len(signatures[sig]) > 1:
+            print(f"Duplicate graphs found with signature {sig}: {[i.name for i in signatures[sig]]}")
+            falsePositive = False
+            for i in range(len(signatures[sig])-1):
+                if (nx.is_isomorphic(signatures[sig][i], signatures[sig][i + 1])):
+                    continue
+                falsePositive = True
+            if falsePositive:
+                fps += 1
+                print("False positive detected!")
+    false_pos.append(fps)
+
+plt.plot(test_v_size, average_time)       # plot y vs x
+plt.xlabel('(|V| + |E|) / 2')      # label x-axis
+plt.ylabel('Time taken to compute 1000 graph hashes')      # label y-axis
+plt.title('Graph Size (|V| + |E|) vs time taken')
+plt.grid(True)       # optional grid
+
+plt.plot(test_v_size, false_pos)       # plot y vs x
+plt.xlabel('(|V| + |E|) / 2')      # label x-axis
+plt.ylabel('Time taken to compute 1000 graph hashes')      # label y-axis
+plt.title('Graph Size (|V| + |E|) vs time taken')
+plt.grid(True)       # optional grid
+plt.show()
+# Isomorphism detection code for validity
