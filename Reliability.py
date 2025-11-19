edf7d8c33b741a5406105be1deae485d53dbb63b
asdfghjkl-alt <56777557+asdfghjkl-alt@users.noreply.github.com>
2025-11-19 16:15:39 +1100

    Tests to compare WFL hash, and reliability

--- Reliability.py
+++ Reliability.py
@@ -5,7 +5,9 @@ import HashUtils
 from random import shuffle, choice
 import matplotlib.pyplot as plt
 import time
-from networkx.algorithms.graph_hashing import weisfeiler_lehman_graph_hash
+import igraph as ig
+
+pedge = 0.80
 
 def shingle(g: nx.Graph):
     nodes = list(g.nodes())
@@ -18,19 +20,12 @@ def shingle(g: nx.Graph):
     # doesn't matter whether shingles are sorted or not
     return shingles
 
-test_v_size = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-
-average_time = []
-false_pos = []
-
-for no_v in test_v_size:
+def gen_graphs(n):
     graphs = []
-    # Running it on 500 graphs to prevent biases in average
-    n = 50
 
     for i in range(n):
-        g = nx.gnp_random_graph(no_v, 0.6)
-        # g = nx.gnm_random_graph(no_v, no_v * 100)
+        # g = nx.random_regular_graph(n = n, d = int(n / 4))
+        g = nx.gnp_random_graph(no_v, pedge)
         g.name = f"graph_{i}"
         graphs.append(g.copy())
         
@@ -39,48 +34,107 @@ for no_v in test_v_size:
     graphs.append(duplicate.copy())  # add a relabelled, duplicated graph
     duplicate = GraphUtils.randomise_labels(original)
     graphs.append(duplicate.copy())  # add a relabelled, duplicated graph
+
     shuffle(graphs)
+    return graphs
 
+test_v_size = [100, 200, 300, 400, 500, 600, 700, 800]
+# test_v_size = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+#  110, 120, 130, 140, 150, 160, 170, 180, 190, 200
+average_time = []
+average_time_wf = []
+false_pos = []
+false_pos_wf = []
+
+for no_v in test_v_size:
+    print("Generating Graphs: ")
+    graphs = gen_graphs(100)
+    print("Performing on " + str(no_v) + " vertices.")
+
+    print("LSH Algorithm Start")
     start = time.perf_counter()
-
     signatures = {}
     for g in graphs:
         shingles = shingle(g)
-        signature = HashUtils.minhash(set(shingles), num_hashes=64)
+        signature = HashUtils.minhash(set(shingles), num_hashes=8)
         if signature not in signatures:
             signatures[signature] = []
         signatures[signature].append(g)
 
     fps = 0
     end = time.perf_counter()
+    print("LSH Algorithm End")
 
     average_time.append(end - start)
-    print(end - start)
+    print("Time: " + str(end - start))
 
     for sig in signatures:
         if len(signatures[sig]) > 1:
             print(f"Duplicate graphs found with signature {sig}: {[i.name for i in signatures[sig]]}")
             falsePositive = False
-            for i in range(len(signatures[sig])-1):
-                if (nx.is_isomorphic(signatures[sig][i], signatures[sig][i + 1])):
+            base = ig.Graph.from_networkx(signatures[sig][0])
+            for i in range(len(signatures[sig])):
+                compare = ig.Graph.from_networkx(signatures[sig][i])
+                if (base.isomorphic(compare)):
                     continue
-                falsePositive = True
-            if falsePositive:
                 fps += 1
+                falsePositive = True
+                break
+            
+            if falsePositive:
                 print("False positive detected!")
     false_pos.append(fps)
 
-plt.subplot(2, 1, 1)
-plt.plot(test_v_size, average_time)
-plt.xlabel('(|V| + |E|) / 2')
-plt.ylabel('Time taken to compute 1000 graph hashes')   
-plt.title('Graph Size (|V| + |E|) vs time taken')
-plt.grid(True)    
+    print("WF Algorithm Start")
+    start = time.perf_counter()
+    signatures = {}
+    for g in graphs:
+        signature = nx.weisfeiler_lehman_graph_hash(g, iterations=3)
+        if signature not in signatures:
+            signatures[signature] = []
+        signatures[signature].append(g)
+    
+    fps = 0
+    end = time.perf_counter()
+    print("WF Algorithm End")
 
-plt.subplot(2, 1, 2)
-plt.plot(test_v_size, false_pos) 
-plt.xlabel('|V|')
-plt.ylabel('Number of False Positives')
-plt.title('Graph no of vertices (|V|) vs False Positives (Probability of edges: 0.6)')
+    average_time_wf.append(end - start)
+    print("Time: " + str(end - start))
+
+    for sig in signatures:
+        if len(signatures[sig]) > 1:
+            print(f"Duplicate graphs found with signature {sig}: {[i.name for i in signatures[sig]]}")
+            falsePositive = False
+            base = ig.Graph.from_networkx(signatures[sig][0])
+            for i in range(len(signatures[sig])):
+                compare = ig.Graph.from_networkx(signatures[sig][i])
+                if (base.isomorphic(compare)):
+                    continue
+                fps += 1
+                falsePositive = True
+                break
+            
+            if falsePositive:
+                print("False positive detected!")
+    false_pos_wf.append(fps)
+
+plt.subplot(1, 2, 1)
+plt.plot([x * (x - 1) / 2 * pedge for x in test_v_size], average_time, label="LSH Hashing")
+plt.plot([x * (x - 1) / 2 * pedge for x in test_v_size], average_time_wf, label="WFL Hash")
+plt.xlabel('|E|')
+plt.ylabel('Time taken to compute 100 graph hashes')   
+plt.title('Graph Size (|E|) vs time taken')
 plt.grid(True)
+plt.legend()
+
+
+plt.subplot(1, 2, 2)
+plt.plot([x * (x - 1) / 2 * pedge for x in test_v_size], false_pos, label="LSH Hashing")
+plt.plot([x * (x - 1) / 2 * pedge for x in test_v_size], false_pos_wf, label="WFL Hash")
+plt.xlabel('|E|')
+plt.ylabel('Amount of false positives')   
+plt.title('Graph Size (|E|) vs no of false positives')
+plt.grid(True)
+plt.legend()
+
 plt.show()
